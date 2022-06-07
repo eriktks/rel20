276ae00b70c322831f810704f2a121977e5e82d3
Erik Tjong Kim Sang <erikt@xs4all.nl>
2022-06-07 17:29:12 +0200

    added bert ner

---
+++ test_bert_md.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+
+from REL.mention_detection import MentionDetection
+
+
+def load_bert_ner(model, tokenizer):
+    try:
+        return pipeline("ner", model=model, tokenizer=tokenizer)
+    except Exception:
+        print("error loading Bert NER model!", file=sys.stderr)
+
+
+def test_md():
+    tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+    model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+    ner_model = load_bert_ner(model, tokenizer)
+
+    md = MentionDetection(Path(__file__).parent, "wiki_test")
+
+    # first test case: repeating sentences
+    sample1 = {"test_doc": ["Fox, Fox. Fox.", []]}
+    resulting_spans1 = {(0, 3), (5, 3), (10, 3)}
+    predictions = md.find_mentions(sample1, ner_model)
+    predicted_spans = []
+    for i in range(0, 1):
+        p = {
+            (m["pos"], m["end_pos"] - m["pos"]) for m in predictions[i]["test_doc"]
+        }
+        predicted_spans.extend(list(p))
+    predicted_spans = set(predicted_spans)
+    assert resulting_spans1 == predicted_spans
+
+    # second test case: excessive whitespace
+    sample2 = {"test_doc": ["Fox,                Fox,                   Fox.", []]}
+    resulting_spans2 = {(0, 3), (20, 3), (43, 3)}
+    predictions = md.find_mentions(sample2, ner_model)
+    predicted_spans = {
+        (m["pos"], m["end_pos"] - m["pos"]) for m in predictions[0]["test_doc"]
+    }
+    assert resulting_spans2 == predicted_spans
